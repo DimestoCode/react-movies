8a4e8ba6e046ee4554842636731e71ab125eef28
DESKTOP-TIB6L9F\Lenovo <andonjevdima@gmail.com>
2021-01-22 00:45:34 +0200

    added project files

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <h3 className="footer-logo">netflixroulette</h3>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
